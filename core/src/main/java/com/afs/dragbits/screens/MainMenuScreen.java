e3e17d02a377de0f5a6a23b6c53de296406e9ed9
Anza Míguez Axel <axelanzamiguez@gmail.com>
2026-09-04 16:57:08 -0300

    controles MainMenuScreen
    se agrega la opcion de controles al menu principal con imagen de referencia

--- core/src/main/java/com/afs/dragbits/screens/MainMenuScreen.java
+++ core/src/main/java/com/afs/dragbits/screens/MainMenuScreen.java
@@ -17,114 +17,192 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenuScreen extends InputAdapter implements Screen {
 
-    private final Main game;
-    private OrthographicCamera camera;
+
+    private final Main juego;
+
+
+    private OrthographicCamera camara;
     private Viewport viewport;
     private SpriteBatch batch;
 
-    private BitmapFont titleFont;
-    private BitmapFont optionFont;
-    private GlyphLayout layout;
 
-    private final String titleText = "DRAG BITS";
-    private final String[] options = {"START GAME", "EXIT"};
-    private int selectedIndex = 0;
+    private BitmapFont fuenteTitulo;
+    private BitmapFont fuenteOpciones;
+    private GlyphLayout layoutTexto;
 
-    private final float NORMAL_SCALE = 2.0f;
-    private final float SELECTED_SCALE = 2.5f;
-    private float[] currentScales = {NORMAL_SCALE, NORMAL_SCALE};
 
-    private float[] optionYPositions = new float[2];
-    private float optionHeight = 60f;
+    private final String textoTitulo = "DRAG BITS";
+    private final String[] opcionesMenu = {"START GAME", "CONTROLES", "EXIT"};
+    private int indiceSeleccionado = 0;
 
-    public MainMenuScreen(Main game) {
-        this.game = game;
 
-        camera = new OrthographicCamera();
-        viewport = new FitViewport(1280f, 720f, camera);
+    private final float ESCALA_NORMAL = 2.0f;
+    private final float ESCALA_SELECCIONADA = 2.5f;
+    private float[] escalasActuales = {ESCALA_NORMAL, ESCALA_NORMAL, ESCALA_NORMAL};
+
+
+    private float[] posicionesYOpciones = new float[3];
+    private final float ALTURA_HITBOX_OPCION = 60f;
+
+
+    private Texture texturaControles;
+    private boolean estaMostrandoControles = false;
+
+    public MainMenuScreen(Main juego) {
+        this.juego = juego;
+
+
+        camara = new OrthographicCamera();
+        viewport = new FitViewport(1280f, 720f, camara);
         batch = new SpriteBatch();
-        layout = new GlyphLayout();
+        layoutTexto = new GlyphLayout();
 
-        titleFont = new BitmapFont();
-        optionFont = new BitmapFont();
+
+        fuenteTitulo = new BitmapFont();
+        fuenteOpciones = new BitmapFont();
+
+
+        texturaControles = new Texture(Gdx.files.internal("Sprites/MenuPrincipal/Controles.png"));
     }
 
     @Override
     public void show() {
+
         Gdx.input.setInputProcessor(this);
 
-        if (game.getMusicaFondo() != null) {
-            game.getMusicaFondo().setVolume(1.0f);
-            if (!game.getMusicaFondo().isPlaying()) {
-                game.getMusicaFondo().play();
+
+        if (juego.getMusicaFondo() != null) {
+            juego.getMusicaFondo().setVolume(1.0f);
+            if (!juego.getMusicaFondo().isPlaying()) {
+                juego.getMusicaFondo().play();
             }
         }
     }
 
     @Override
     public void render(float delta) {
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        for (int i = 0; i < options.length; i++) {
-            float targetScale = (i == selectedIndex) ? SELECTED_SCALE : NORMAL_SCALE;
-            currentScales[i] = MathUtils.lerp(currentScales[i], targetScale, delta * 12f);
-        }
 
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
+        camara.update();
+        batch.setProjectionMatrix(camara.combined);
         batch.begin();
 
-        titleFont.getData().setScale(4.0f);
-        layout.setText(titleFont, titleText);
-        float titleX = (viewport.getWorldWidth() - layout.width) / 2;
-        float titleY = viewport.getWorldHeight() * 0.75f;
-        titleFont.draw(batch, titleText, titleX, titleY);
 
-        float startY = viewport.getWorldHeight() * 0.45f;
-        float spacing = 80f;
-
-        for (int i = 0; i < options.length; i++) {
-            optionFont.getData().setScale(currentScales[i]);
-            layout.setText(optionFont, options[i]);
-
-            float x = (viewport.getWorldWidth() - layout.width) / 2;
-            float y = startY - (i * spacing);
-            optionYPositions[i] = y;
-
-            optionFont.draw(batch, options[i], x, y);
-
-            if (i == selectedIndex) {
-                float arrowX = x - 60f;
-                optionFont.draw(batch, ">", arrowX, y);
-            }
+        if (estaMostrandoControles) {
+            dibujarPantallaControles();
+        } else {
+            dibujarMenuPrincipal(delta);
         }
 
         batch.end();
     }
 
+
+    private void dibujarPantallaControles() {
+
+        float anchoTextura = texturaControles.getWidth();
+        float altoTextura = texturaControles.getHeight();
+
+
+        float centroX = (viewport.getWorldWidth() - anchoTextura) / 2f;
+        float centroY = (viewport.getWorldHeight() - altoTextura) / 2f;
+
+
+        batch.draw(texturaControles, centroX, centroY, anchoTextura, altoTextura);
+
+
+        fuenteOpciones.getData().setScale(ESCALA_NORMAL);
+        layoutTexto.setText(fuenteOpciones, "MENU");
+
+        float margenDerecho = 30f;
+        float margenSuperior = 30f;
+        float botonMenuX = viewport.getWorldWidth() - layoutTexto.width - margenDerecho;
+        float botonMenuY = viewport.getWorldHeight() - margenSuperior;
+
+        fuenteOpciones.draw(batch, "MENU", botonMenuX, botonMenuY);
+    }
+
+    private void dibujarMenuPrincipal(float delta) {
+
+        for (int i = 0; i < opcionesMenu.length; i++) {
+            float escalaObjetivo = (i == indiceSeleccionado) ? ESCALA_SELECCIONADA : ESCALA_NORMAL;
+            escalasActuales[i] = MathUtils.lerp(escalasActuales[i], escalaObjetivo, delta * 12f);
+        }
+
+
+        fuenteTitulo.getData().setScale(4.0f);
+        layoutTexto.setText(fuenteTitulo, textoTitulo);
+        float tituloX = (viewport.getWorldWidth() - layoutTexto.width) / 2;
+        float tituloY = viewport.getWorldHeight() * 0.75f;
+        fuenteTitulo.draw(batch, textoTitulo, tituloX, tituloY);
+
+
+        float posicionYInicial = viewport.getWorldHeight() * 0.45f;
+        float espacioEntreOpciones = 80f;
+
+        for (int i = 0; i < opcionesMenu.length; i++) {
+            fuenteOpciones.getData().setScale(escalasActuales[i]);
+            layoutTexto.setText(fuenteOpciones, opcionesMenu[i]);
+
+            float opcionX = (viewport.getWorldWidth() - layoutTexto.width) / 2;
+            float opcionY = posicionYInicial - (i * espacioEntreOpciones);
+
+
+            posicionesYOpciones[i] = opcionY;
+
+            fuenteOpciones.draw(batch, opcionesMenu[i], opcionX, opcionY);
+
+
+            if (i == indiceSeleccionado) {
+                float flechaX = opcionX - 60f;
+                fuenteOpciones.draw(batch, ">", flechaX, opcionY);
+            }
+        }
+    }
+
     @Override
     public boolean keyDown(int keycode) {
+        if (estaMostrandoControles) {
+
+            if (keycode == Input.Keys.ESCAPE || keycode == Input.Keys.ENTER) {
+                estaMostrandoControles = false;
+            }
+            return true;
+        }
+
+
         if (keycode == Input.Keys.UP) {
-            selectedIndex--;
-            if (selectedIndex < 0) selectedIndex = options.length - 1;
+            indiceSeleccionado--;
+            if (indiceSeleccionado < 0) {
+                indiceSeleccionado = opcionesMenu.length - 1;
+            }
         } else if (keycode == Input.Keys.DOWN) {
-            selectedIndex++;
-            if (selectedIndex >= options.length) selectedIndex = 0;
+            indiceSeleccionado++;
+            if (indiceSeleccionado >= opcionesMenu.length) {
+                indiceSeleccionado = 0;
+            }
         } else if (keycode == Input.Keys.ENTER) {
-            executeSelectedOption();
+            ejecutarOpcionSeleccionada();
         }
         return true;
     }
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        Vector3 worldCoords = viewport.unproject(new Vector3(screenX, screenY, 0));
+        if (!estaMostrandoControles) {
 
-        for (int i = 0; i < options.length; i++) {
-            float y = optionYPositions[i];
-            if (worldCoords.y <= y && worldCoords.y >= y - optionHeight) {
-                selectedIndex = i;
+            Vector3 coordenadasMundo = viewport.unproject(new Vector3(screenX, screenY, 0));
+
+            for (int i = 0; i < opcionesMenu.length; i++) {
+                float topeY = posicionesYOpciones[i];
+                float baseY = topeY - ALTURA_HITBOX_OPCION;
+
+                if (coordenadasMundo.y <= topeY && coordenadasMundo.y >= baseY) {
+                    indiceSeleccionado = i;
+                }
             }
         }
         return true;
@@ -133,17 +211,39 @@ public class MainMenuScreen extends InputAdapter implements Screen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         if (button == Input.Buttons.LEFT) {
-            executeSelectedOption();
+            if (estaMostrandoControles) {
+
+                Vector3 coordenadasMundo = viewport.unproject(new Vector3(screenX, screenY, 0));
+
+                float areaBotonX = viewport.getWorldWidth() - 200f;
+                float areaBotonY = viewport.getWorldHeight() - 100f;
+
+                if (coordenadasMundo.x > areaBotonX && coordenadasMundo.y > areaBotonY) {
+                    estaMostrandoControles = false;
+                }
+            } else {
+                ejecutarOpcionSeleccionada();
+            }
         }
         return true;
     }
 
-    private void executeSelectedOption() {
-        if (selectedIndex == 0) {
-            game.setScreen(new MapaScreen(game));
-            this.dispose();
-        } else if (selectedIndex == 1) {
-            Gdx.app.exit();
+    private void ejecutarOpcionSeleccionada() {
+
+        switch (indiceSeleccionado) {
+            case 0:
+
+                juego.setScreen(new MapaScreen(juego));
+                this.dispose();
+                break;
+            case 1:
+
+                estaMostrandoControles = true;
+                break;
+            case 2:
+
+                Gdx.app.exit();
+                break;
         }
     }
 
@@ -159,11 +259,14 @@ public class MainMenuScreen extends InputAdapter implements Screen {
 
     @Override
     public void dispose() {
+
         batch.dispose();
-        titleFont.dispose();
-        optionFont.dispose();
+        fuenteTitulo.dispose();
+        fuenteOpciones.dispose();
+        texturaControles.dispose();
     }
 
     @Override public void pause() {}
     @Override public void resume() {}
 }
+
